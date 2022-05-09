7d2cc0df67606872c526043205d24e42616efe15
Michalis Kargakis <kargakis@protonmail.com>
2022-05-09 11:16:50 +0200

    Drop dotenv and extra ABI

--- utils/addresses.ts
+++ utils/addresses.ts
@@ -1,5 +1,3 @@
-require("dotenv").config();
-
 interface IfcOneNetworksAddresses {
   bct: string;
   nct: string;
@@ -15,12 +13,12 @@ const addresses: IfcAddresses = {
   polygon: {
     bct: "0x2F800Db0fdb5223b3C3f354886d907A671414A7F",
     nct: "0xD838290e877E0188a4A44700463419ED96c16107",
-    offsetHelper: process.env.OFFSET_HELPER_ADDRESS || "",
+    offsetHelper: "0xfc50e90d7DF550d9428B15Dd8e1bB4931778Dfbf",
   },
   mumbai: {
     bct: "0xf2438A14f668b1bbA53408346288f3d7C71c10a1",
     nct: "0x7beCBA11618Ca63Ead5605DE235f6dD3b25c530E",
-    offsetHelper: process.env.OFFSET_HELPER_ADDRESS || "",
+    offsetHelper: "0x9910788B676Cee48476B10982DD754B8D55F2c2c",
   },
 };
 

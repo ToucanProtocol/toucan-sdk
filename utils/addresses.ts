42ab3b67e4bcab608b26dd53034d5da0f8b1f225
Alex Lazar <alex2012ro@gmail.com>
2022-05-15 01:39:51 +0300

    chore: updated offsetHelper address

--- utils/addresses.ts
+++ utils/addresses.ts
@@ -13,12 +13,12 @@ const addresses: IfcAddresses = {
   polygon: {
     bct: "0x2F800Db0fdb5223b3C3f354886d907A671414A7F",
     nct: "0xD838290e877E0188a4A44700463419ED96c16107",
-    offsetHelper: "0xfc50e90d7DF550d9428B15Dd8e1bB4931778Dfbf",
+    offsetHelper: "0xb334795bf50e4943d076Dfb38D8C1A50F9F5a101",
   },
   mumbai: {
     bct: "0xf2438A14f668b1bbA53408346288f3d7C71c10a1",
     nct: "0x7beCBA11618Ca63Ead5605DE235f6dD3b25c530E",
-    offsetHelper: "0x9910788B676Cee48476B10982DD754B8D55F2c2c",
+    offsetHelper: "0x3E0E589cBd008024Cd272418Ea44Dfc4940650d3",
   },
 };
 

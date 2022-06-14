30dff38f26c7f6df06b38eaf2885fe25b75b6e4c
Alex Lazar <alex2012ro@gmail.com>
2022-06-15 00:34:42 +0300

    refactor: no need for address in constructor
    - remove the need for the user to provide his signer's address
    - removed signerish & providerish
    - allow more provider types
    - updated docs
    
    We can now fetch the signer's address with `await this.signer.getAddress();`.

--- types/index.ts
+++ types/index.ts
@@ -1,12 +1,4 @@
-import { ethers } from "ethers";
-
 export type Network = "polygon" | "mumbai";
-
-export type providerish =
-  | ethers.providers.Web3Provider
-  | ethers.providers.JsonRpcProvider;
-
-export type signerish = ethers.providers.Provider | ethers.Signer;
 
 export type poolSymbol = "BCT" | "NCT";
 

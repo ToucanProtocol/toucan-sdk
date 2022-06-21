ddb11accba53ec74d07fb49af29220680c1682ad
Alex Lazar <alex2012ro@gmail.com>
2022-06-21 21:16:23 +0300

    refactor: graph api urls not hardcoded

--- utils/graphClients.ts
+++ utils/graphClients.ts
@@ -1,16 +1,23 @@
 import { createClient } from "@urql/core";
 
 import { Network } from "../types";
-import { MUMBAI_GRAPH_API_URL, POLYGON_GRAPH_API_URL } from "./graphAPIs";
+import {
+  MUMBAI_TOUCAN_GRAPH_API_URL,
+  POLYGON_SUSHI_GRAPH_API,
+  POLYGON_TOUCAN_GRAPH_API_URL,
+} from "./graphAPIs";
 
 export const getToucanGraphClient = (network: Network) =>
   createClient({
-    url: network == "polygon" ? POLYGON_GRAPH_API_URL : MUMBAI_GRAPH_API_URL,
+    url:
+      network == "polygon"
+        ? POLYGON_TOUCAN_GRAPH_API_URL
+        : MUMBAI_TOUCAN_GRAPH_API_URL,
     requestPolicy: "network-only",
     fetch: fetch,
   });
 
 export const getSushiGraphClient = () =>
   createClient({
-    url: "https://api.thegraph.com/subgraphs/name/sushiswap/matic-exchange",
+    url: POLYGON_SUSHI_GRAPH_API,
   });

aa2fc69d792ed427449b0ecc3db3261554dd47a2
Alex Lazar <alex2012ro@gmail.com>
2022-06-22 16:20:38 +0300

    refactor: constant naming
    Co-authored-by: Michalis Kargakis <kargakis@protonmail.com>

--- utils/graphAPIs.ts
+++ utils/graphAPIs.ts
@@ -2,5 +2,5 @@ export const POLYGON_TOUCAN_GRAPH_API_URL =
   "https://api.thegraph.com/subgraphs/name/toucanprotocol/matic";
 export const MUMBAI_TOUCAN_GRAPH_API_URL =
   "https://api.thegraph.com/subgraphs/name/toucanprotocol/mumbai";
-export const POLYGON_SUSHI_GRAPH_API =
+export const POLYGON_SUSHI_GRAPH_API_URL =
   "https://api.thegraph.com/subgraphs/name/sushiswap/matic-exchange";
--- utils/graphClients.ts
+++ utils/graphClients.ts
@@ -3,7 +3,7 @@ import { createClient } from "@urql/core";
 import { Network } from "../types";
 import {
   MUMBAI_TOUCAN_GRAPH_API_URL,
-  POLYGON_SUSHI_GRAPH_API,
+  POLYGON_SUSHI_GRAPH_API_URL,
   POLYGON_TOUCAN_GRAPH_API_URL,
 } from "./graphAPIs";
 
@@ -19,5 +19,5 @@ export const getToucanGraphClient = (network: Network) =>
 
 export const getSushiGraphClient = () =>
   createClient({
-    url: POLYGON_SUSHI_GRAPH_API,
+    url: POLYGON_SUSHI_GRAPH_API_URL,
   });
